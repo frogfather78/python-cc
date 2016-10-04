0c580bc8f5f013f816dc017ef743194898631879
frogfather78 <frogfather@gmail.com>
2016-10-04 22:45:12 +0100

    Update bikes.py

--- bikes.py
+++ bikes.py
@@ -1,3 +1,3 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized', 'state']
+bicycles = ['trek', 'cannondale', 'redline', 'specialized', 'raleigh']
 message="My first bicycle was a "+bicycles[1].title()+"."
 print(message)
