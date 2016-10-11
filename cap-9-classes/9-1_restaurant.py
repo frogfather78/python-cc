f6b081fca78d3b8b40bb7712ed55b795afa7af6c
frogfather78 <frogfather@gmail.com>
2016-10-11 20:32:04 +0100

    methods1

--- cap-9-classes/9-1_restaurant.py
+++ cap-9-classes/9-1_restaurant.py
@@ -1,4 +1,5 @@
 #9-1 restaurant
+#9-4 number served
 
 class Restaurant():
 	"""modeling a restaurant, kinda"""
@@ -7,6 +8,7 @@ class Restaurant():
 		"""initialise some stuff"""
 		self.name = name
 		self.cuisine = cuisine
+		self.number_served = 0
 	
 	def describe_restaurant(self):
 		print("This place called " + self.name.title() +
@@ -14,6 +16,12 @@ class Restaurant():
 			
 	def open_restuarant(self):
 		print(self.name.title() + " is now open!")
+		
+	def set_number_served(self, number_served):
+		self.number_served = number_served
+		
+	def increment_number_served(self, increment):
+		self.number_served += increment
 
 restaurant_0 = Restaurant('morleys', 'chicken')
 
@@ -25,7 +33,19 @@ restaurant_0.describe_restaurant()
 restaurant_2.describe_restaurant()
 restaurant_1.describe_restaurant()
 
+restaurant_1.set_number_served(5)
 
+print(restaurant_1.number_served)
+
+
+restaurant_1.set_number_served(19)
+
+print(restaurant_1.number_served)
+
+restaurant_1.increment_number_served(4)
+
+print(restaurant_1.name.title() + " has served " + 
+	str(restaurant_1.number_served) + " peeps today")
 #restaurant.open_restuarant()
 
 		
--- cap-9-classes/9-3_user_profiles.py
+++ cap-9-classes/9-3_user_profiles.py
@@ -1,5 +1,6 @@
 #9-3 user profiles
 #again
+#9-5 login attempts
 
 class User():
 	"""modeling a user of a something"""
@@ -8,6 +9,7 @@ class User():
 		self.first_name = first
 		self.last_name = last
 		self.user_id = uid
+		self.login_attempts = 0
 	
 	def describe_user(self):
 		print(self.first_name.title() + " " + self.last_name.title() +
@@ -16,6 +18,14 @@ class User():
 	
 	def greet_user(self):
 		print("Hey, " + self.first_name.title() + "! Welcome.")
+		print("You have attempted " + str(self.login_attempts) +
+			" logins to this thing.")
+		
+	def increment_login_attempts(self):
+		self.login_attempts += 1
+	
+	def reset_login_attempts(self):
+		self.login_attempts = 0 
 	
 bert = User("bert", "welch", 1)
 
@@ -27,4 +37,16 @@ bert.describe_user()
 
 ernie.greet_user()
 ernie.describe_user()
+
+ernie.increment_login_attempts()
+ernie.increment_login_attempts()
+ernie.increment_login_attempts() #3
+
+ernie.greet_user()
+
+ernie.reset_login_attempts()
+
+print("Reset Ernie's login attempts")
+ernie.greet_user()
+
 user_x.greet_user()
