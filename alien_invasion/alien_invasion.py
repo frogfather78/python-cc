921528b7e0a546ec34f9d4b0b758320100f77115
frogfather78 <frogfather@gmail.com>
2016-10-29 15:10:03 +0100

    alien fleet
    adding aliens and making them move across the screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+
 import game_functions as gf
 
 def run_game():
@@ -13,19 +14,21 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-	
-	#make a ship
+
+	#make a ship, a group of bullet and a group of aliens
 	ship = Ship(ai_settings,screen)
-	
-	#make a group to store bullets in
 	bullets = Group()
-	
+	aliens = Group()
+
+	#create the fleet of aliens
+	gf.create_fleet(ai_settings, screen, ship, aliens)
+
 	#set the game loop
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
 		gf.update_bullets(bullets)
-		
-		gf.update_screen(ai_settings, screen, ship, bullets)
-	
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,14 +7,19 @@ class Settings():
 		self.screen_width = 1200
 		self.screen_height = 600
 		self.bg_colour = (0, 0, 255)
-		
+
 		#ship settings
 		self.ship_speed_factor = 1.5
-		
+
+		#alien settings
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		#fleet_direction of 1 means go right, -1 means go left
+		self.fleet_direction = 1
+
 		#bullet settings
 		self.bullet_speed_factor = 1
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_colour = (60, 60, 60)
 		self.bullets_allowed = 3
-
